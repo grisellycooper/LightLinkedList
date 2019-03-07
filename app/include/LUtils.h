5e99ec34da81dbc5d354846ba66a5bb54304e5d4
Griselly Ramos <griselly.ramos.c@gmail.com>
2019-03-07 17:00:14 -0500

    Late First commit

---
+++ app/include/LUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace LUtils
+{
+    class A
+    {
+        public:
+            void print();
+    };
+}
+
---
+++ app/src/LUtils.cpp
@@ -0,0 +1,13 @@
+
+#include "../include/LCommon.h"
+#include "../include/LUtils.h"
+
+
+using namespace LUtils;
+
+void A::print()
+{
+    cout << "A was created" << endl;
+}
+
+//void A::print(){}
